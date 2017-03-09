76cf03c8a7a1aa83f3a368f9a0c917e3a42d1cab
Dani Adkins <danirenaeadkins12@gmail.com>
2017-03-09 12:25:37 -0600

    adding partials and auth

--- app/app.js
+++ app/app.js
@@ -1,1 +1,38 @@
-'use strict';
+"use strict";
+
+var app = angular.module("Bangazon", ['ngRoute'])
+    .constant('apiUrl', 'http://localhost:8000/api');
+
+angular.module("Bangazon").config(
+  [
+      'interpolateProvider',
+      '$httpProvider',
+      '$routeProvider',
+      function($interpolateProvider, $httpProvider, $routeProvider) {
+
+        $interpolateProvider.startSymbol('{{');
+        $interpolateProvider.endSymbol('}}');
+
+        $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+        $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+        $httpProvider.defaults.withCredentials = true;
+
+        $routeProvider
+          .when('/', {
+            controller: "AuthCtrl",
+            templateUrl: 'bangazon/auth/login.html'
+          })
+          .when('/register', {
+            controller: "AuthCtrl",
+            templateUrl: 'bangazon/auth/register.html'
+          })
+          .when('/addProduct', {
+            templateUrl: 'partials/createProduct.html',
+            controller:'addProductCtrl'
+          })
+          .when('/addProductType', {
+            templateUrl:'partials/addProductType.html',
+            controller: 'addProductTypeCtrl'
+          });
+      }
+  ]);
---
+++ app/controllers/AuthCtrl.js
@@ -0,0 +1,64 @@
+"use strict";
+
+app.controller("AuthCtrl", [
+  '$scope',
+  '$http',
+  '$location',
+  'RootFactory',
+function($scope, $http, $location, RootFactory) {
+
+  $scope.user = {
+    username: "tinyhands",
+    password: "tinyhands",
+    email: "tinyhands@tinyhands.com",
+    first_name: "Donald",
+    last_name: "Trump"
+  };
+
+  $scope.register = function() {
+    $http({
+      url: "http://localhost:8000/register",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      data: {
+        "username": $scope.user.username,
+        "password": $scope.user.password,
+        "email": $scope.user.email,
+        "first_name": $scope.user.first_name,
+        "last_name": $scope.user.last_name,
+      }
+    }).then(
+      res => {
+        RootFactory.setToken(res.data.token);
+        console.log(RootFactory.getToken());
+        if (res.data.success === true) {
+          $location.path('/landing');
+        }
+      },
+      console.error
+    );
+  };
+
+  $scope.login = function() {
+    $http({
+      url: "http://localhost:8000/api-token-auth",
+      method: "POST",
+      data: {
+        "username": $scope.user.username,
+        "password": $scope.user.password
+      }
+    }).then(
+      res => {
+        RootFactory.setToken(res.data.token);
+        console.log(RootFactory.getToken());
+        if (res.data.token !== "") {
+          $location.path('/landing');
+        }
+       },
+        console.error
+      );
+    };
+    }
+  ]);
