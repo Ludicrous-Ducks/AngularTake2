b01d58b68c7438ac63ab28e4a4f36ea2b4ad4d1e
juliakimchung <julhkm@gmail.com>
2017-03-09 13:07:08 -0600

    basic app.js done

--- app/app.js
+++ app/app.js
@@ -1,1 +1,26 @@
 'use strict';
+
+let app = angular.module("Bangazon", ['ngRoute'])
+    .constant('apiUrl', 'http://localhost:7000/api');
+
+angular.module('BangaClient').config(
+[
+    '$interpolateProvider',
+    '$routeProvider',
+    function($interpolateProvider, $routeProvider) {
+
+      $interpolateProvider.startSymbol('((');
+      $interpolateProvider.endSymbol('))');
+
+      $routeProvider
+      	.when('/', {
+      		controller: 'AuthController',
+      		templateUrl: 'partials/login.html'
+      	});
+
+
+
+		}
+
+]);
+
