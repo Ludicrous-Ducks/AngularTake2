d45b992949cb5cdfcb9cd46b96d44d80421f140b
Shawn M <forgeforza@outlook.com>
2017-03-10 16:26:38 -0600

    navbar routing working with login

--- app/app.js
+++ app/app.js
@@ -4,30 +4,34 @@ var app = angular.module("Bangazon", ['ngRoute'])
     .constant('apiUrl', 'http://localhost:7000/api');
 
 angular.module("Bangazon").config(
-  [
-      '$interpolateProvider',
-      '$httpProvider',
-      '$routeProvider',
-      function($interpolateProvider, $httpProvider, $routeProvider) {
+    [
+        '$interpolateProvider',
+        '$httpProvider',
+        '$routeProvider',
+        '$locationProvider',
+        function($interpolateProvider, $httpProvider, $routeProvider, $locationProvider) {
 
-        $interpolateProvider.startSymbol('{{');
-        $interpolateProvider.endSymbol('}}');
+            $interpolateProvider.startSymbol('{{');
+            $interpolateProvider.endSymbol('}}');
 
-        $httpProvider.defaults.xsrfCookieName = 'csrftoken';
-        $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+            $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+            $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
 
-        $routeProvider
-          .when('/login', {
-            controller: "AuthCtrl",
-            templateUrl: 'partials/auth.html'
-          })
-          .when('/register', {
-            controller: "AuthCtrl",
-            templateUrl: 'partials/auth.html'
-          })
-          .when('/landing', {
-            controller: "AuthCtrl",
-            templateUrl: 'partials/landing.html'
-          })
-      }
-  ]);
+            $routeProvider
+                .when('/login', {
+                    controller: "AuthCtrl",
+                    templateUrl: 'partials/auth.html'
+                })
+                .when('/register', {
+                    controller: "AuthCtrl",
+                    templateUrl: 'partials/auth.html'
+                })
+                .when('/main', {
+                    controller: "AuthCtrl",
+                    templateUrl: 'partials/main.html'
+                })
+                .otherwise('/');
+
+            $locationProvider.html5Mode(true);
+        }
+    ]);
